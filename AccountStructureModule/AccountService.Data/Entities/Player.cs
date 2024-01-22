14edb10dfaeb05f196d1665017d3e8fb6b66f7f5
yskah <yskah@YAVUZ>
2024-01-22 08:38:13 +0300

    Add project files.

---
+++ AccountStructureModule/AccountService.Data/Entities/Player.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountService.Data.Entities
+{
+    public class Player
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; } = null!;
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public string FirstName { get; set; } = null!;
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public string LastName { get; set; } = null!;
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public string Username { get; set; } = null!;
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.Int32)]
+        public int Point { get; set; }
+
+    }
+}
---
+++ AccountStructureModule/AccountService.Data/Repositories/PlayerRepository.cs
@@ -0,0 +1,23 @@
+﻿using AccountService.Data.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountService.Data.Repositories
+{
+    public class PlayerRepository
+    {
+        private readonly IMongoCollection<Player> playerCollection;
+        public PlayerRepository()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+
+            var database = client.GetDatabase("AccountStructureDb");
+
+            this.playerCollection = database.GetCollection<Player>("players");
+        }
+    }
+}
