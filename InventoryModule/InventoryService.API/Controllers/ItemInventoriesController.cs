7a180e37fc96db9e56b9756f1259758741a100f1
Yavuz Selim KAHRAMAN <yskproje@gmail.com>
2024-04-21 19:21:54 +0300

    last

---
+++ InventoryModule/InventoryService.API/Controllers/ItemInventoriesController.cs
@@ -0,0 +1,55 @@
+﻿using InventoryService.API.Dtos;
+using InventoryService.Data.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemInventoriesController : ControllerBase
+    {
+        private readonly ItemInventoryRepository _itemInventoryRepository;
+
+        public ItemInventoriesController(ItemInventoryRepository itemInventoryRepository)
+        {
+            _itemInventoryRepository = itemInventoryRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var list = await  _itemInventoryRepository.GetAll();
+            return Ok(list);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ItemInventoryCreateDto dto)
+        {
+            var result = await _itemInventoryRepository.Create(new Data.Entities.ItemInventory
+            {
+                Count = dto.Count,
+                InventoryId = dto.InventoryId,
+                ItemId = dto.ItemId,
+            });
+
+            return Created("", result);
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> Update(ItemInventoryUpdateDto dto)
+        {
+            var updatedEntity = await _itemInventoryRepository.GetById(dto.Id);
+            if(updatedEntity != null)
+            {
+                updatedEntity.Count =updatedEntity.Count - dto.Count;
+                await _itemInventoryRepository.Update(updatedEntity);
+                return NoContent();
+            }
+            return NotFound();
+          
+        }
+
+    }
+}
--- InventoryModule/InventoryService.API/Dtos/InventoryDtos.cs
+++ InventoryModule/InventoryService.API/Dtos/InventoryDtos.cs
@@ -1,4 +1,7 @@
 ﻿namespace InventoryService.API.Dtos
 {
-  public record InventoryCreateDto(string PlayerId, string Name);
+    public record InventoryCreateDto(string PlayerId, string Name);
+    public record ItemInventoryCreateDto(string InventoryId, string ItemId, int Count);
+
+    public record ItemInventoryUpdateDto(string Id, int Count);
 }
